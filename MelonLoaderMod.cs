fe752210d680396d7ea4a87157bb55f3688b98b2
DarkSwitchPro <59634615+DarkSwitchPro@users.noreply.github.com>
2021-11-30 01:53:08 -0500

    BW-BetterLaunching

---
+++ MelonLoaderMod.cs
@@ -0,0 +1,54 @@
+﻿using MelonLoader;
+using UnityEngine;
+using StressLevelZero.Props.Weapons;
+using HarmonyLib;
+
+namespace BetterLaunching
+{
+    public static class BuildInfo
+    {
+        public const string Name = "BetterLaunching";
+        public const string Author = "DarkSwitchPro";
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class BetterLaunching : MelonMod
+    {
+        [HarmonyPatch(typeof(GravityGun), "Blast")]
+        public static class GravityGunBlastPatch
+        {
+            public static Vector3 preVelocity;
+
+            public static void Prefix(GravityGun __instance)
+            {
+                preVelocity = __instance.m_GrabbedRigidbody.gameObject.GetComponent<Rigidbody>().velocity;
+            }
+
+            public static void Postfix(GravityGun __instance)
+            {
+                Vector3 postVelocity = __instance.m_GrabbedRigidbody.gameObject.GetComponent<Rigidbody>().velocity;
+
+                Rigidbody grabbedRigidbody = __instance.m_GrabbedRigidbody;
+                Transform root = grabbedRigidbody.transform.root;
+                Rigidbody[] rigidbodies = root.GetComponentsInChildren<Rigidbody>();
+                float velocityImpulseFactor = 2f;
+
+                foreach (Rigidbody rb in rigidbodies)
+                {
+                    if (rb != null)
+                    {
+                        rb.AddForce(postVelocity * velocityImpulseFactor, ForceMode.Impulse);
+                    }
+                }
+            }
+        }
+
+        public override void OnApplicationStart()
+        {
+            HarmonyLib.Harmony harmonyInstance = new HarmonyLib.Harmony("DSP.BetterLaunching.Harmony");
+            harmonyInstance.PatchAll();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(BetterLaunching.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(BetterLaunching.BuildInfo.Company)]
+[assembly: AssemblyProduct(BetterLaunching.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + BetterLaunching.BuildInfo.Author)]
+[assembly: AssemblyTrademark(BetterLaunching.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(BetterLaunching.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(BetterLaunching.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(BetterLaunching.BetterLaunching), BetterLaunching.BuildInfo.Name, BetterLaunching.BuildInfo.Version, BetterLaunching.BuildInfo.Author, BetterLaunching.BuildInfo.DownloadLink)]
+[assembly: MelonGame(null, null)]
